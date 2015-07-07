63e39764832dc06026c06c838689914008c9f17a
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2015-07-07 23:30:39 +0300

    Try to fix some readthedocs issues

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,8 @@
 import sys
 import os
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -157,7 +159,13 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'default'
-html_theme = 'sphinx_rtd_theme'
+
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'sphinx_rtd_theme'
+    #html_theme = 'nature'
+
 #html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
