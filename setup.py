2255eb6fd362779dabe5e5bcbf630a5917e7522a
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2017-06-21 16:16:30 +0300

    Preparing release 0.10.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setupopts = dict(
     name="automate",
-    version='0.10.2.dev0',
+    version='0.10.2',
     packages=find_packages('src'),
     include_package_data=True,
     package_dir={'': 'src'},
