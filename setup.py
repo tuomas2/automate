f82b1a15f4e447d7a4856449cf678e0cbb55f578
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2017-06-22 12:43:38 +0300

    Back to development: 0.10.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setupopts = dict(
     name="automate",
-    version='0.10.3',
+    version='0.10.4.dev0',
     packages=find_packages('src'),
     include_package_data=True,
     package_dir={'': 'src'},
