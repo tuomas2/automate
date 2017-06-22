484e6bcb5780be0c41c5e944669b12787b4bbbc1
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2017-06-22 12:16:29 +0300

    GPIO: Fix input port setup

--- examples/gpiotest.py
+++ examples/gpiotest.py
@@ -3,7 +3,7 @@ from automate import *
 
 class mysys(System):
     s = RpioSensor(port=22, buttontype='down')
-    a = RpioActuator(port=23, changedelay=2)
+    a = RpioActuator(port=23, change_delay=2)
     p = Program(active_condition=Value(s),
                 on_activate=SetStatus(a, True))
 
