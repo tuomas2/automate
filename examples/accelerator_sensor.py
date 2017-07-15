ec25b0932c6db57c52038a6820d7e43d21892cd5
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2017-07-15 18:45:17 +0300

    New example examples/accelerator_sensor.py

---
+++ examples/accelerator_sensor.py
@@ -0,0 +1,40 @@
+from automate import *
+from automate.extensions.arduino import ArduinoAnalogSensor, ArduinoService
+from automate.extensions.webui import WebService
+
+
+val_list = []
+count = 0
+def plot(x, y, z):
+    global count
+    val_list.append((x, y, z))
+    print('value', count, x, y, z)
+    count += 1
+    if count % 1000 == 0:
+        print('writing to file')
+        with open('dump.txt', 'w') as f:
+            for i, (x, y, z) in enumerate(val_list):
+                f.write(f'{i} {x} {y} {z}\n')
+
+
+class Motion(System):
+    x = ArduinoAnalogSensor(dev=0, pin=0,
+                            active_condition=Value(True),
+                            update_condition=Value(True),
+                            on_update=Func(plot, 'x', 'y', 'z'),
+                            exclude_triggers={'y', 'z'}
+                            )
+    y = ArduinoAnalogSensor(dev=0, pin=1)
+    z = ArduinoAnalogSensor(dev=0, pin=2)
+
+
+s = Motion(
+    services=[
+        ArduinoService(
+            arduino_devs=["/dev/ttyUSB0"],
+            arduino_dev_types=["Arduino"],
+            arduino_dev_sampling=[100],
+        ),
+        WebService(),
+    ],
+)
