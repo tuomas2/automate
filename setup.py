95f502029a8afb6354071479794e57da00e115fa
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2017-07-15 21:41:13 +0300

    Preparing release 0.10.15

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ all_extras_requirements = web_requirements + gpio_requirements + arduino_require
 
 setupopts = dict(
     name="automate",
-    version='0.10.15.dev0',
+    version='0.10.15',
     packages=find_packages('src'),
     include_package_data=True,
     package_dir={'': 'src'},
