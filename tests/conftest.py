85dd3312f3517609e5db04525dcdc905fa39a165
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2017-07-16 20:28:04 +0300

    Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,13 @@
 from __future__ import unicode_literals
-from automate.test_utils import *
+
+from pytest_capturelog import CaptureLogHandler
+import pytest
+import logging
+
+from automate.test_utils import sysloader, check_log
+
+@pytest.fixture(scope='function')
+def caplog(caplog):
+    logger = logging.getLogger('automate')
+    logger.addHandler(CaptureLogHandler())
+    yield caplog
