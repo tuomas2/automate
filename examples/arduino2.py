1002e15776612a69f1cf91880887330b7872e6b0
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2017-07-20 21:39:15 +0300

    examples: Modify arduino2.py to use VirtualWire stuff

--- examples/arduino2.py
+++ examples/arduino2.py
@@ -1,25 +1,34 @@
 from automate import *
-from automate.extensions.arduino import ArduinoDigitalSensor, ArduinoDigitalActuator, \
+from automate.extensions.arduino import ArduinoDigitalActuator, ArduinoPWMActuator, \
     ArduinoService, ArduinoVirtualWireActuator, ArduinoVirtualWireSensor
 from automate.extensions.webui import WebService
-from automate.program import Program
 
 
 class mysys(System):
-    #config = Config(print_level = logging.DEBUG)
-    # Control servo with analog port a1 through interpolating sensor interp
-    ustr = UserStrSensor()
+    ustr0 = UserStrSensor()
+    ustr1 = UserStrSensor()
     ubool = UserBoolSensor()
+    ufloat1 = UserFloatSensor(value_min=0, value_max=1)
 
-    d13 = ArduinoDigitalActuator(dev=0, pin=13)  # LED on Arduino board
+    d13_0 = ArduinoDigitalActuator(dev=0, pin=13,
+                                   on_update=SetStatus('d13_0', 'ubool'))
+    d13_1 = ArduinoDigitalActuator(dev=1, pin=13,
+                                   on_update=SetStatus('d13_1', 'ubool'))
 
-    vwactuator = ArduinoVirtualWireActuator(dev=1, pin=10)
-    #d11 = ArduinoDigitalSensor(dev=0, pin=11)
-    vwsensor = ArduinoVirtualWireSensor(dev=0, pin=10)
-
-    prog = Program(
-        on_update=Run(SetStatus(d13, ubool), SetStatus(vwactuator, ustr))
+    vwsensor0 = ArduinoVirtualWireSensor(dev=0, pin=10)
+    vwactuator0 = ArduinoVirtualWireActuator(dev=0, pin=11,
+        on_update=SetStatus('vwactuator0', ustr0)
     )
+
+    vwsensor1 = ArduinoVirtualWireSensor(dev=1, pin=10)
+    vwactuator1 = ArduinoVirtualWireActuator(dev=1, pin=11,
+        on_update=SetStatus('vwactuator1', ustr1)
+    )
+
+    # Using PWM on pins 9/10 will break VirtualWire.
+    # pwmactuator1 = ArduinoPWMActuator(dev=0, pin=9,
+    #   on_update=SetStatus('pwmactuator1', ufloat1)
+    #)
 
 s = mysys(
     services=[ArduinoService(
