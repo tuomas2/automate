733bf8fdd99a8dd3f70f32a1701f5c892ef17fd9
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2017-07-25 17:23:37 +0300

    Improve examples/arduino2

--- examples/arduino2.py
+++ examples/arduino2.py
@@ -114,74 +114,76 @@ class mysys2(System):
     ustr0 = UserStrSensor()
     ustr1 = UserStrSensor()
     ubool = UserBoolSensor()
+    class Input(Group):
+        event1 = UserEventSensor(
+            on_activate=VirtualWireMessage(0, target_dev, 'test test')
+        )
+        event2 = UserEventSensor(
+            on_activate=SetVirtualPin(0, target_dev, 1, 5)
+        )
 
-    event1 = UserEventSensor(
-        on_activate=VirtualWireMessage(0, target_dev, 'test test')
-    )
-    event2 = UserEventSensor(
-        on_activate=SetVirtualPin(0, target_dev, 1, 5)
-    )
+        event3 = UserEventSensor(
+            on_activate=VirtualWireCommand(0, target_dev, arduino_service.VIRTUALWIRE_SET_VIRTUAL_PIN_VALUE, 1, arduino_service.TYPE_STR, "test")
+        )
 
-    event3 = UserEventSensor(
-        on_activate=VirtualWireCommand(0, target_dev, arduino_service.VIRTUALWIRE_SET_VIRTUAL_PIN_VALUE, 1, arduino_service.TYPE_STR, "test")
-    )
-
-    event4 = UserEventSensor(
-        on_activate=VirtualWireCommand(0, target_dev, arduino_service.VIRTUALWIRE_SET_VIRTUAL_PIN_VALUE, 1, arduino_service.TYPE_FLOAT, arduino_service.float_to_bytes(0.5))
-    )
-
-
-    reset1 = UserEventSensor(
-        on_activate=FirmataCommand(0, pyfirmata.SYSTEM_RESET)
-    )
-    reset2 = UserEventSensor(
-        on_activate=FirmataCommand(1, pyfirmata.SYSTEM_RESET)
-    )
-
-    ubool2 = UserBoolSensor(
-        on_update=VirtualWireCommand(0, target_dev, arduino_service.VIRTUALWIRE_SET_DIGITAL_PIN_VALUE, 13, 'ubool2')
-    )
-
-    ufloat1 = UserFloatSensor(value_min=0, value_max=1)
-    ufloat2 = UserFloatSensor(value_min=0, value_max=1)
-
-    local_pwm = ArduinoPWMActuator(dev=0, pin=3, on_update=SetStatus('local_pwm', 'ufloat2'))
+        event4 = UserEventSensor(
+            on_activate=VirtualWireCommand(0, target_dev, arduino_service.VIRTUALWIRE_SET_VIRTUAL_PIN_VALUE, 1, arduino_service.TYPE_FLOAT, arduino_service.float_to_bytes(0.5))
+        )
 
 
-#    d13_0 = ArduinoDigitalActuator(dev=0, pin=13,
-#                                   on_update=SetStatus('d13_0', 'ubool'))
-#    d13_1 = ArduinoDigitalActuator(dev=1, pin=13,
-#                                   on_update=SetStatus('d13_1', 'ubool'))
+        reset1 = UserEventSensor(
+            on_activate=FirmataCommand(0, pyfirmata.SYSTEM_RESET)
+        )
+        reset2 = UserEventSensor(
+            on_activate=FirmataCommand(1, pyfirmata.SYSTEM_RESET)
+        )
 
-#    vwsensor0 = ArduinoVirtualWireMessageSensor(dev=0, pin=10)
+        ubool2 = UserBoolSensor(
+            on_update=VirtualWireCommand(0, target_dev, arduino_service.VIRTUALWIRE_SET_DIGITAL_PIN_VALUE, 13, 'ubool2')
+        )
 
-    remote_actuator = ArduinoRemoteDigitalActuator(dev=0, target_device=target_dev, target_pin=12,
-                    on_update=SetStatus('remote_actuator', 'ubool'))
+        ufloat1 = UserFloatSensor(value_min=0, value_max=1)
+        ufloat2 = UserFloatSensor(value_min=0, value_max=1)
 
-    remote_pwm = ArduinoRemotePWMActuator(dev=0, target_device=target_dev, target_pin=5,
-                                          on_update=SetStatus('remote_pwm', 'ufloat1'))
+    class Local(Group):
+        local_pwm = ArduinoPWMActuator(dev=0, pin=3, on_update=SetStatus('local_pwm', 'ufloat2'))
 
-    source_sens1 = ArduinoDigitalSensor(dev=0, pull_up_resistor=True, pin=2) # Makes subscription
-    source_sens2 = ArduinoAnalogSensor(dev=0, pin=0) # Makes subscription
 
-    awds1 = ArduinoBroadcastDigitalSensor(dev=1, source_device=source_dev, pin=2) # receives via VW
-    awds2 = ArduinoBroadcastAnalogSensor(dev=1, source_device=source_dev, pin=0) # receives via VW
+    #    d13_0 = ArduinoDigitalActuator(dev=0, pin=13,
+    #                                   on_update=SetStatus('d13_0', 'ubool'))
+    #    d13_1 = ArduinoDigitalActuator(dev=1, pin=13,
+    #                                   on_update=SetStatus('d13_1', 'ubool'))
 
-    vwactuator0 = ArduinoVirtualWireMessageActuator(
-        dev=0,
-        recipient=target_dev,
-        # TODO these settings shoudl be per device
-        on_update=SetStatus('vwactuator0', ustr0),
-    )
+    #    vwsensor0 = ArduinoVirtualWireMessageSensor(dev=0, pin=10)
 
-    vwsensor1a = ArduinoVirtualWireMessageSensor(dev=1)
-    vwsensor1b = ArduinoVirtualWireMessageSensor(dev=1)
+        remote_actuator = ArduinoRemoteDigitalActuator(dev=0, target_device=target_dev, target_pin=12,
+                        on_update=SetStatus('remote_actuator', 'ubool'))
 
-    vwsensor_vpin1 = VirtualPinSensor(dev=1, virtual_pin=1)
-    vwsensor_vpin2 = VirtualPinSensor(dev=1, virtual_pin=2)
-#    vwactuator1 = ArduinoVirtualWireMessageActuator(dev=1, pin=11,
-#        on_update=SetStatus('vwactuator1', ustr1)
-#    )
+        remote_pwm = ArduinoRemotePWMActuator(dev=0, target_device=target_dev, target_pin=5,
+                                              on_update=SetStatus('remote_pwm', 'ufloat1'))
+
+        source_sens1 = ArduinoDigitalSensor(dev=0, pull_up_resistor=True, pin=2) # Makes subscription
+        source_sens2 = ArduinoAnalogSensor(dev=0, pin=0) # Makes subscription
+
+    class Remote(Group):
+        awds1 = ArduinoBroadcastDigitalSensor(dev=1, source_device=source_dev, pin=2) # receives via VW
+        awds2 = ArduinoBroadcastAnalogSensor(dev=1, source_device=source_dev, pin=0) # receives via VW
+
+        vwactuator0 = ArduinoVirtualWireMessageActuator(
+            dev=0,
+            recipient=target_dev,
+            # TODO these settings shoudl be per device
+            on_update=SetStatus('vwactuator0', 'ustr0'),
+        )
+
+        vwsensor1a = ArduinoVirtualWireMessageSensor(dev=1)
+        vwsensor1b = ArduinoVirtualWireMessageSensor(dev=1)
+
+        vwsensor_vpin1 = VirtualPinSensor(dev=1, virtual_pin=1)
+        vwsensor_vpin2 = VirtualPinSensor(dev=1, virtual_pin=2)
+    #    vwactuator1 = ArduinoVirtualWireMessageActuator(dev=1, pin=11,
+    #        on_update=SetStatus('vwactuator1', ustr1)
+    #    )
 
 
 s = mysys2(
