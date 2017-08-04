92476115f7ce6c9d2158cef0ff4c13ad6de77099
Tuomas Airaksinen <tuomas.airaksinen@gmail.com>
2017-08-04 14:26:04 +0300

    Back to development: 0.10.20

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ all_extras_requirements = web_requirements + gpio_requirements + arduino_require
 
 setupopts = dict(
     name="automate",
-    version='0.10.19',
+    version='0.10.20.dev0',
     packages=find_packages('src'),
     include_package_data=True,
     package_dir={'': 'src'},
